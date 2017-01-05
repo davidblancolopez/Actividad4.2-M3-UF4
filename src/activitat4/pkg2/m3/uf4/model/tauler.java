e228faef72574c8c018899c021960b8f920435fa
davidblancolopez <daviddbl94@gmail.com>
2017-01-05 19:19:03 +0100

    Cambios

--- src/activitat4/pkg2/m3/uf4/model/tauler.java
+++ src/activitat4/pkg2/m3/uf4/model/tauler.java
@@ -6,19 +6,51 @@ package activitat4.pkg2.m3.uf4.model;
  * @author dblan
  */
 public class tauler {
-    private boolean estat;
-    private String color;
+    
+    private casella [][] tauler;
+    int num1, num2;
+
+    public tauler(int num1, int num2) {
+        this.tauler = new casella [num1][num2];
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+
+    
 
     
     
     
-    public tauler(boolean estat, String color) {
-        this.estat = estat;
-        this.color = color;
+    
+    class casella{
+        boolean estat;
+        String color;
+
+        public casella(boolean estat, String color) {
+            this.estat = estat;
+            this.color = color;
+        }
+
+        public boolean getEstat() {
+            return estat;
+        }
+
+        public void setEstat(boolean estat) {
+            this.estat = estat;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+        
+        
+        
     }
-    
-    
-    
     
     
     
