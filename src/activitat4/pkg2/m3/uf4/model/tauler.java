deb373bd17d91198638b2fe1a9e2bf161fe966fd
davidblancolopez <daviddbl94@gmail.com>
2017-01-05 18:50:34 +0100

    Cambios

--- src/activitat4/pkg2/m3/uf4/model/tauler.java
+++ src/activitat4/pkg2/m3/uf4/model/tauler.java
@@ -6,5 +6,20 @@ package activitat4.pkg2.m3.uf4.model;
  * @author dblan
  */
 public class tauler {
+    private boolean estat;
+    private String color;
+
+    
+    
+    
+    public tauler(boolean estat, String color) {
+        this.estat = estat;
+        this.color = color;
+    }
+    
+    
+    
+    
+    
     
 }
