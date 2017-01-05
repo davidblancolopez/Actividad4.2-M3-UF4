2f71108e795a56cbd578f25991261242f9096ce7
davidblancolopez <daviddbl94@gmail.com>
2017-01-05 19:50:15 +0100

    Cambios

--- src/control/controlTauler.java
+++ src/control/controlTauler.java
@@ -1,10 +1,24 @@
 
 package control;
 
+import vista.vistaTauler;
+
 /**
  *
  * @author dblan
  */
 public class controlTauler {
+    private vistaTauler vText;
+
+    public controlTauler() {
+        this.vText = vText;
+    }
     
+        
+    public void respostaMenu(){
+        
+    }
+
+
+
 }
--- src/vista/vistaTauler.java
+++ src/vista/vistaTauler.java
@@ -3,19 +3,53 @@ package vista;
 
 
 import activitat4.pkg2.m3.uf4.model.tauler;
+import java.util.Scanner;
 /**
  *
  * @author dblan
  */
 public class vistaTauler {
+    
+    private Scanner lector = new Scanner(System.in);
+    
+    
     /**
      * Metode per a mostrar el menú de la aplicació amb les opcions.
      */
-    public void mostrarMenu(){
+    public int mostrarMenu(){
+        int opcio;
+        
+        do{
         System.out.println("Menú: \n"
                 + "1. Modificar casella. \n"
                 + "2. Sortir.");
+        opcio = lector.nextInt();
+        }while(comprovarMenu(opcio));
+        
+        return opcio;
     }
+    
+    
+    /**
+     * Metode per a comprovar que la opcio introduida al menu es correcte.
+     *
+     * @param opcio
+     * @return
+     */
+    public boolean comprovarMenu(int opcio) {
+        boolean comprovar = true;
+
+        //Si es compleix la condició el boolean serà False, per a qe així pugui sortir del bucle que es troba en "menu".
+        if (opcio > 0 && opcio < 3) {
+            comprovar = false;
+        }
+
+        return comprovar;
+    }
+
+    
+    
+    
     
     /**
      * Metode que serveix per a mostrar el tauler
@@ -29,4 +63,8 @@ public class vistaTauler {
             }
         }
     }
+    
+    
+    
+    
 }
